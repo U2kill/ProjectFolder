c6d9e5808242c8a0f9991eda8b0017186a9be148
Артем <pisosik1337@gmail.com>
2025-11-29 20:13:58 +0300

    Продолжаем ООП

--- Main.py
+++ Main.py
@@ -10,11 +10,9 @@ from JES import Jes
 from typing import Union, List
 import inspect
 
-filePath = None
-savePath = None
-# textPath = []
-log = []
-SWCTname = "Value"
+
+
+
 
 
 
@@ -22,133 +20,132 @@ class MyApp(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.textPath = []
+
+        self.filePath = None
+        self.textPath = None
+        self.operation = "SWCT creator"
+
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.operation = "SWCT creator"
-        self.appStat = AppStat(self.ui)
-        self.changeUI = ChangeUi(self.ui)
         self.log = Logger(self.ui)
+        self.appStat = AppStat(self.ui, self.log)
+        self.longTask = LongTask(self.appStat, self.log)
+        self.changeUI = ChangeUi(self.ui)
+
         self.ui.comboBox.currentTextChanged.connect(self.changeUI.comboBoxFunc)
         self.ui.pushButton.clicked.connect(self.openFileDialog)
         self.ui.pushButton_2.clicked.connect(self.openSaveFileDialog)
         self.ui.lineEdit.editingFinished.connect(self.saveName)
         self.ui.pushButton_3.clicked.connect(self.startOp)
-        self.threadpool = QThreadPool()
+
+        
 
     def openFileDialog(self):
         if self.ui.comboBox.currentText() == "SWCT Creator":
-            # global textPath
             curFilePath, _ = QFileDialog.getOpenFileNames(self,"Выберите файл","",";Текстовые (*.txt);")
             if len(curFilePath) > 0:
                 self.textPath = self.appStat.status(curFilePath)
-                # textPath = curFilePath[:]
                 self.ui.pushButton.setText(f"{Path(curFilePath[0]).parent}")
                 self.log.addLog(f"Добавлены файлы: \n{"\n".join(curFilePath)}")
-                # print(textPath)
         
         else:
-            global filePath
             curFilePath, _ = QFileDialog.getOpenFileName(self, "Выберите файл", "", "Текстовые (*.xlsm)")
             if len(curFilePath) > 0:
-                dir = Path(curFilePath)
-                filePath = curFilePath
-                self.ui.pushButton.setText(dir.stem)
-                log.append(f"Добавлен файл SWCT: \n{curFilePath}")
-                self.logger(log)
+                self.filePath = self.appStat.status(curFilePath)
+                self.ui.pushButton.setText(Path(curFilePath).stem)
+                self.log.addLog(f"Добавлен файл SWCT: \n{curFilePath}")
+
 
 
     def openSaveFileDialog(self):
-        global savePath
         curFilePath = QFileDialog.getExistingDirectory(None, "Выберите папку", "/home/user")
         if len(curFilePath) > 2:
-            savePath = curFilePath
-            self.ui.pushButton_2.setText(savePath)
-            log.append(f"Добавлена директория для сохранения документа: \n{savePath}")
-            self.logger(log)
-
-    def logger(self, log):
-        for i in log:
-            str(i)
-        log = "\n".join(log)
-        self.ui.textEdit.setText(log)
+            self.savePath = self.appStat.status(curFilePath)
+            self.ui.pushButton_2.setText(self.savePath)
+            self.log.addLog(f"Добавлена директория для сохранения документа: \n{self.savePath}")
 
     def saveName(self):
         if self.appStat.nameChecker() == True:
             self.ui.lineEdit.setStyleSheet("")
-            global SWCTname
-            SWCTname = f"{self.ui.lineEdit.text().strip()}.xlsm"
-            print(SWCTname)
-            log.append(f"Добавлено имя файла: \n{SWCTname}")
-            self.logger(log)
+            self.SWCTname = f"{self.ui.lineEdit.text().strip()}.xlsm"
+            self.log.addLog(f"Добавлено имя файла: \n{self.SWCTname}")
         
     def startOp(self):
         if self.ui.comboBox.currentText() == "SWCT Creator":
-            if self.appStat.nameChecker() == True and self.appStat.filePathChecker(self.textPath, self.operation) == True:
-                print(self.textPath)
-                result = Text(self.textPath, SWCTname, savePath)
-                log.append("Создание документа...")
-                self.logger(log)
-                self.threadpool.start(result)
-                result.signals.progress.connect(lambda txt:(log.append(f"{txt}"), self.logger(log)))
-                result.signals.finished.connect(lambda: (log.append(f"\nСоздан файл: {SWCTname}"), self.logger(log)))
+            self.longTask.createSWCT(self.textPath, self.savePath, self.SWCTname)
+            # if self.appStat.nameChecker() == True and self.appStat.filePathChecker(self.textPath, self.savePath) == True:
+            #     result = Text(self.textPath, self.SWCTname, self.savePath)
+            #     self.log.addLog("Создание документа...")
+            #     self.threadpool.start(result)
+            #     result.signals.progress.connect(lambda txt:(self.log.addLog(f"{txt}")))
+            #     result.signals.finished.connect(lambda: (self.log.addLog(f"\nСоздан файл: {self.SWCTname}")))
 
-            elif self.appStat.nameChecker() == False:
-                self.appStat.namError()
+            # elif self.appStat.nameChecker() == False:
+            #     self.appStat.namError()
 
         elif self.ui.comboBox.currentText() == "Yamazumi Creator":
-            if self.filePathChecker() == True:
-                log.append("Создание документа...")
-                self.logger(log)
-                result = Yamazumi(filePath, savePath)
+            if self.appStat.filePathChecker(self.filePath, self.savePath) == True:
+                self.log.addLog("Создание документа...")
+                result = Yamazumi(self.filePath, self.savePath)
                 self.threadpool.start(result)
-                result.signals.finished.connect(lambda: (log.append("\nСоздан файл: Yamazumi цеха.xlsx"), self.logger(log)))
+                result.signals.finished.connect(lambda: (self.log.addLog("\nСоздан файл: Yamazumi цеха.xlsx")))
     
         elif self.ui.comboBox.currentText() == "JES Creator":
-            if self.filePathChecker() == True:
-                log.append("Создание документа...")
-                self.logger(log)
-                result = Jes(filePath, savePath)
+            if self.appStat.filePathChecker(self.filePath, self.savePath) == True:
+                self.log.addLog("Создание документа...")
+                result = Jes(self.filePath, self.savePath)
                 self.threadpool.start(result)
-                result.signals.finished.connect(lambda: (log.append("\nСоздан файл: Jes.xlsx"), self.logger(log)))
+                result.signals.finished.connect(lambda: (self.log.addLog("\nСоздан файл: Jes.xlsx")))
 
 
 class AppStat:
-    def __init__(self, ui):
+    def __init__(self, ui, log):
         self.ui = ui
+        self.log = log
 
     def status(self, value):
         callerFrame = inspect.stack()[1]
         callerName = callerFrame.function
 
         if callerName == "openFileDialog":
+            return self.FileDialog(value)
+        
+        if callerName == "openSaveFileDialog":
+            return value
+
+    def FileDialog(self, value):
             try:
                 if Path(value[0]).suffix == ".txt":
                     textPath = value
                     return textPath
-            
             except:
                 pass
-            
 
-    def nameChecker(self): # Добавить нормальную проверку имени файла
+            try: 
+                if Path(value).suffix == ".xlsm":
+                    filePath = value
+                    return filePath
+            except:
+                pass        
+
+
+    def nameChecker(self): # Добавить нормальную проверку имени светильника
         if len(str(self.ui.lineEdit.text())) == 0:
             return False
         else:
             return True
-    
-    def filePathChecker(self, textPath, operation):
-        if operation == "SWCT creator":
-            if textPath == None or savePath == None:
-                return False
-            else:
-                return True
 
-        if operation == "Yamazumi Creator" or operation == "JES Creator":
-            if filePath == None or savePath == None:
-                return False
-            else:
-                return True    
+        
+    def filePathChecker(self, *paths: Union[str, List[str]]):
+        try:
+            for path in paths:
+                if len(path) == 0:
+                    return False
+            return True
+        except TypeError:
+
+            QApplication.beep()
+            self.log.addLog("Ошибка: Не выбраны все пути файлов")
 
     def namError(self):
         QApplication.beep()
@@ -188,12 +185,22 @@ class Logger:
         self.uiLog = "\n".join(self.logger)
         self.ui.textEdit.setText(self.uiLog)
 
+class LongTask:
+    def __init__(self, stat, log):
+        self.appStat = stat
+        self.log = log
+        self.threadpool = QThreadPool()
 
+    def createSWCT(self, textPath: List[str], savePath: str, SWCTname: str):
+        if self.appStat.nameChecker() == True and self.appStat.filePathChecker(textPath, savePath) == True:
+            result = Text(textPath, SWCTname, savePath)
+            self.log.addLog("Создание документа...")
+            self.threadpool.start(result)
+            result.signals.progress.connect(lambda txt:(self.log.addLog(f"{txt}")))
+            result.signals.finished.connect(lambda: (self.log.addLog(f"\nСоздан файл: {SWCTname}")))
 
-
-
-
-# class FileManager:
+        elif self.appStat.nameChecker() == False:
+            self.appStat.namError()
     
 
 
@@ -201,6 +208,6 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyApp()
     window.show()
-    app.exec_()
+    app.exec()
 
 myString = True
